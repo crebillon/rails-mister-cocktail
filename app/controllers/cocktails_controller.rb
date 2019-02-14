1262a227ef7d8d6f05dea1ac768158d820abaa16
Charlotte Rebillon <charlotte_rebillon@orange.fr>
2019-02-14 16:12:31 +0100

    features: create cocktail/ ingredients

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,31 @@
+class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+    @doses = Dose.all
+    @ingredients = Ingredient.all
+  end
+
+  def new
+    @cocktail = Cocktail.new
+    @dose = Dose.new
+  end
+
+  def create
+    @cocktail = Cocktail.create(cocktails_params)
+    if @cocktail.save
+      redirect_to cocktails_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def cocktails_params
+    params.require(:cocktail).permit(:name)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+puts "Deleting old database"
+Ingredient.destroy_all
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+drinks_serialized = open(url).read
+drinks = JSON.parse(drinks_serialized)
+
+puts 'Creating ingredients...'
+drinks["drinks"].each do |ing|
+  Ingredient.create!(name: ing["strIngredient1"])
+end
+
+puts 'Finished!'
